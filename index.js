2b98bc2ea02e50c6079849b936838893b2764775
kamumi <113558333+kamumi@users.noreply.github.com>
2022-09-14 21:38:13 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const cover = document.querySelector(".card-image");
+const title = document.querySelector(".card-content h5");
+const artist = document.querySelector(".artist");
+const audio = document.querySelector("audio");
+
